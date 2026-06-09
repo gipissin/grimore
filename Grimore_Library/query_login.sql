abf73a688fd0b90ebb12ea2102e85c012d926c4e
Giovanna Vinturi <gigipissinatovinturi@hotmail.com>
2026-06-09 20:54:14 -0300

    Excluindo o arquivo antigo query_login.sql

--- Grimore_Library/query_login.sql
+++
@@ -1,8 +0,0 @@
-create database projeto_grimore;
-use projeto_grimore
-create table tb_usuarios(
-matricula int not null primary key,
-senha varchar(8),
-pergunta_seg varchar(100))
-
-select * from tb_usuarios
